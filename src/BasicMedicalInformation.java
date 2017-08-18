d8325f233ddce700f323e5777336317f66167429
Yiheng Shu <lzw429@qq.com>
2017-08-18 16:09:49 +0800

    GUI init

--- src/BasicMedicalInformation.java
+++ src/BasicMedicalInformation.java
@@ -1,7 +1,15 @@
 import javax.swing.*;
 
 public class BasicMedicalInformation {
-    private JPanel mainPanel;
-    private JTabbedPane tabbedPane1 ;
+    private JPanel mainpanel;
+    private JTabbedPane tabbedPane1;
     private JPanel medicine;
+    private JPanel treatment;
+    private JPanel serviceFacilities;
+    private JPanel fixedMedicalInstitution;
+    private JPanel diseases;
+    private JPanel medicalTreatmentCalculationParameters;
+    private JTextField medicineCodingText;
+    private JLabel medicineCoding;
+
 }
